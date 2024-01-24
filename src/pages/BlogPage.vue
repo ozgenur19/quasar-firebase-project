678dc394ba3d461cbbb66c60769f8ae2bcabd094
özgenur bilican <ozgenurblcn@gmail.com>
2024-01-24 03:59:43 +0300

    commit9
    Blog

---
+++ src/pages/BlogPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><TheHeader /></div>
+  <div><BlogPage /></div>
+  <div><TheFooter /></div>
+</template>
+
+<script setup>
+import TheHeader from 'components/TheComp1.vue';
+import BlogPage from 'components/TheComp7.vue';
+import TheFooter from 'components/TheComp5.vue';
+</script>
