2ac9ecaa453028b4c32655fdae17492c92fe4ba8
özgenur bilican <ozgenurblcn@gmail.com>
2024-01-24 03:58:58 +0300

    commit8
    sipariş takip

---
+++ src/pages/SiparişTakipPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><TheHeader /></div>
+  <div><SiparişTakipPage /></div>
+  <div><TheFooter /></div>
+</template>
+
+<script setup>
+import TheHeader from 'components/TheComp1.vue';
+import SiparişTakipPage from 'components/TheComp6.vue';
+import TheFooter from 'components/TheComp5.vue';
+</script>
