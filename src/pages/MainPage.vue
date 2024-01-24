c9033edf604ac4b690b67c655ebd10c420bcaacc
özgenur bilican <ozgenurblcn@gmail.com>
2024-01-24 03:55:29 +0300

    commit4
    main page

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><TheHeader /></div>
+  <div><Carousel1 /></div>
+  <div style="height: 1200px"></div>
+  <div><Carousel2 /></div>
+  <div style="height: 500px"></div>
+  <div><TheFooter /></div>
+</template>
+
+<script setup>
+import TheHeader from 'components/TheComp1.vue';
+import Carousel1 from 'components/TheComp9.vue';
+import Carousel2 from 'components/TheComp10.vue';
+import TheFooter from 'components/TheComp5.vue';
+</script>
