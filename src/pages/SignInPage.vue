e9f8f7c63bdd0664ca82bfd20d2c152444803733
özgenur bilican <ozgenurblcn@gmail.com>
2024-01-24 03:57:19 +0300

    commit6
    üye girişi

---
+++ src/pages/SignInPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><TheHeader /></div>
+  <div><SignInPage /></div>
+  <div><TheFooter /></div>
+</template>
+
+<script setup>
+import TheHeader from 'components/TheComp1.vue';
+import SignInPage from 'components/TheComp8.vue';
+import TheFooter from 'components/TheComp5.vue';
+</script>
