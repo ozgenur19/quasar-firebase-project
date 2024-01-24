4f45fb915d1e4ba3a55c35993c7af1e05bf64f8f
özgenur bilican <ozgenurblcn@gmail.com>
2024-01-24 03:56:13 +0300

    commit5
    sepet page

---
+++ src/pages/SepetPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><TheHeader /></div>
+  <div><SepetPage /></div>
+  <div><TheFooter /></div>
+</template>
+
+<script setup>
+import TheHeader from 'components/TheComp1.vue';
+import SepetPage from 'components/TheComp4.vue';
+import TheFooter from 'components/TheComp5.vue';
+</script>
