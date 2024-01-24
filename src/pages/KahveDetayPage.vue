f42b3682d08e84eee4dabaf00b635877cda555ac
özgenur bilican <ozgenurblcn@gmail.com>
2024-01-24 03:58:23 +0300

    commit7
    kahve detay

---
+++ src/pages/KahveDetayPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><TheHeader /></div>
+  <div><KahveDetay /></div>
+  <div><TheFooter /></div>
+</template>
+
+<script setup>
+import TheHeader from 'components/TheComp1.vue';
+import KahveDetay from 'components/TheComp3.vue';
+import TheFooter from 'components/TheComp5.vue';
+</script>
